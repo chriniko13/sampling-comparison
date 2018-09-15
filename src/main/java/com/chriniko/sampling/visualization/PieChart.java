386291af24ed38933505119faa98a167358797ee
Nikolaos Christidis <nick.christidis@glispamedia.com>
2018-09-15 10:22:46 +0300

    minor

--- src/main/java/com/chriniko/sampling/visualization/PieChart.java
+++ src/main/java/com/chriniko/sampling/visualization/PieChart.java
@@ -3,11 +3,14 @@ package com.chriniko.sampling.visualization;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.labels.PieSectionLabelGenerator;
+import org.jfree.chart.labels.StandardPieSectionLabelGenerator;
 import org.jfree.chart.plot.PiePlot3D;
 import org.jfree.data.general.PieDataset;
 import org.jfree.util.Rotation;
 
 import javax.swing.*;
+import java.text.DecimalFormat;
 
 public class PieChart extends JFrame {
 
@@ -24,6 +27,10 @@ public class PieChart extends JFrame {
     private JFreeChart createChart(PieDataset dataset, String title) {
         JFreeChart chart = ChartFactory.createPieChart3D(title, dataset, true, true, false);
         PiePlot3D plot = (PiePlot3D) chart.getPlot();
+
+        PieSectionLabelGenerator gen = new StandardPieSectionLabelGenerator(
+                "{0}: {1} ({2})", new DecimalFormat("0"), new DecimalFormat("0%"));
+        plot.setLabelGenerator(gen);
         plot.setStartAngle(290);
         plot.setDirection(Rotation.CLOCKWISE);
         plot.setForegroundAlpha(0.5f);
